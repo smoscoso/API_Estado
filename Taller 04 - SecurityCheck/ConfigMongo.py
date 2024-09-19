e540f53096f3aa6ecd667ea80ddba88b7bdb285c
smoscoso <143634080+smoscoso@users.noreply.github.com>
2024-09-19 18:50:43 -0500

    Delete Taller 04 - SecurityCheck directory

--- Taller 04 - SecurityCheck/ConfigMongo.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-
-config={
-    "URI":"mongodb+srv://smoscoso:Sergio_M10S@cluster0.v6amn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    "DATABASE":"Ciencia_de_Datos",
-    "COLLECTION":"CyberSecurity"
-}
